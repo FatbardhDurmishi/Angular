99cfea84fc7155f64005cf101798eee246d9644c
Fatbardh Durmishi <fdurmishi789@gmail.com>
2023-08-09 09:45:47 +0200

    SortUsersOnClick

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,6 +12,12 @@ export class UsersComponent {
   frontEndUsers: User[] = [];
   backEndUsers: User[] = [];
   fullStackUsers: User[] = [];
+  arrowClassNames: string[] = [
+    'fa-solid fa-sort-down',
+    'fa-solid fa-sort-down',
+    'fa-solid fa-sort-down',
+  ];
+  sortingStates: boolean[] = [true, true, true];
 
   constructor(private userService: UserService) {
     this.updateUserData();
@@ -26,15 +32,41 @@ export class UsersComponent {
     this.Users = this.userService.getUsers();
     this.frontEndUsers = this.Users.filter(
       (user) => user.jobPosition.toLowerCase() === 'frontend'
-    );
+    ).sort((a, b) => a.userName.localeCompare(b.userName));
     this.backEndUsers = this.Users.filter(
       (user) => user.jobPosition.toLowerCase() === 'backend'
-    );
+    ).sort((a, b) => a.userName.localeCompare(b.userName));
     this.fullStackUsers = this.Users.filter(
       (user) => user.jobPosition.toLowerCase() === 'fullstack'
-    );
+    ).sort((a, b) => a.userName.localeCompare(b.userName));
   }
-  // <i class="fa-solid fa-sort-up"></i>
+
+  // sortUsersByUsername(users: User[]) {
+  //   debugger;
+  //   if (users.length >= 2) {
+  //     if (arrayIsAsscending) {
+  //       this.arrowClassName = 'fa-solid fa-sort-up';
+  //       users.sort((a, b) => b.userName.localeCompare(a.userName));
+  //     } else {
+  //       this.arrowClassName = 'fa-solid fa-sort-down';
+  //       users.sort((a, b) => a.userName.localeCompare(b.userName));
+  //     }
+  //     this.arrayIsAsscending = !arrayIsAsscending;
+  //   }
+  // }
+
+  sortUsersByUsername(users: User[], tableIndex: number) {
+    if (users.length >= 2) {
+      if (this.sortingStates[tableIndex]) {
+        this.arrowClassNames[tableIndex] = 'fa-solid fa-sort-up';
+        users.sort((a, b) => b.userName.localeCompare(a.userName));
+      } else {
+        this.arrowClassNames[tableIndex] = 'fa-solid fa-sort-down';
+        users.sort((a, b) => a.userName.localeCompare(b.userName));
+      }
+      this.sortingStates[tableIndex] = !this.sortingStates[tableIndex];
+    }
+  }
 
   trackByUserId(index: number, user: User): number {
     return user.id; //
